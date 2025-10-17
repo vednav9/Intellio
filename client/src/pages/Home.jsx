454903d43996ccce543be09bb8164c68badc39e8
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-17 18:44:45 +0530

    add Testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Navbar />
       <Hero />
       <AiTools />
+      <Testimonial />
     </>
   )
 }
